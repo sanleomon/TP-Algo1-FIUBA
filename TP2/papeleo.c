914e5960ac75d82f8daf35cedb5af15710ef2865
sanleomon <170890793+sanleomon@users.noreply.github.com>
2026-01-08 04:16:39 +0000

    Add pre- and post-conditions to functions in papeleo.c

--- TP2/papeleo.c
+++ TP2/papeleo.c
@@ -18,6 +18,10 @@ const char MIKE = 'W';
 #define TERRENO_NIVEL_2 17
 #define TERRENO_NIVEL_3 12
 
+/* Pre-condiciones: tope_paredes >= 0
+*  Post-condiciones: Devuelve true si coordenada coincide exactamente con alguna pared, 
+*       false en caso contrario.
+*/
 bool superpone_con_pared(coordenada_t coordenada, coordenada_t* paredes, int tope_paredes){
     int i = 0;
     bool superpuesto = false;
@@ -30,6 +34,10 @@ bool superpone_con_pared(coordenada_t coordenada, coordenada_t* paredes, int top
     return superpuesto;
 }
 
+/* Pre-condiciones: tope_paredes >= 0
+*  Post-condiciones: Devuelve true si coordenada está a distancia de Manhattan 1 de alguna pared,
+*       false en caso contrario.
+*/
 bool es_adyacente_a_pared(coordenada_t coordenada, coordenada_t* paredes, int tope_paredes){
     int i = 0;
     bool adyacente = false;
@@ -48,6 +56,10 @@ bool es_adyacente_a_pared(coordenada_t coordenada, coordenada_t* paredes, int to
     return adyacente;
 }
 
+/* Pre-condiciones: tope_obstaculos >= 0
+*  Post-condiciones: Devuelve true si coordenada coincide exactamente con algún obstáculo,
+*       false en caso contrario.
+*/
 bool superpone_con_obstaculo(coordenada_t coordenada, objeto_t* obstaculos, int tope_obstaculos){
     int i = 0;
     bool superpuesto = false;
@@ -61,10 +73,18 @@ bool superpone_con_obstaculo(coordenada_t coordenada, objeto_t* obstaculos, int 
     return superpuesto;
 }
 
+/* Pre-condiciones: -
+*  Post-condiciones: Devuelve true si coordenada coincide exactamente con la posición del jugador,
+*       false en caso contrario.
+*/
 bool superpone_con_jugador(coordenada_t pos_jugador, coordenada_t coordenada){
     return (pos_jugador.fil == coordenada.fil && pos_jugador.col == coordenada.col);
 }
 
+/* Pre-condiciones: tope_herramientas >= 0
+*  Post-condiciones: Devuelve true si coordenada coincide con alguna herramienta del nivel,
+*       false en caso contrario.
+*/
 bool superpone_con_herramienta(coordenada_t coordenada, objeto_t* herramientas, 
         int tope_herramientas){
     int i = 0;
@@ -79,6 +99,10 @@ bool superpone_con_herramienta(coordenada_t coordenada, objeto_t* herramientas,
     return superpuesto;
 }
 
+/* Pre-condiciones: tope_papeleos >= 0
+*  Post-condiciones: Devuelve true si coordenada coincide con algún papeleo del nivel,
+*       false en caso contrario.
+*/
 bool superpone_con_papeleo(coordenada_t coordenada, papeleo_t* papeleo, 
         int tope_papeleo){
     int i = 0;
@@ -93,6 +117,10 @@ bool superpone_con_papeleo(coordenada_t coordenada, papeleo_t* papeleo,
     return superpuesto;
 }
 
+/* Pre-condiciones: nivel ya inicializado con sus paredes y posición inicial del jugador.
+*      cantidad_fuegos >= 0 y tamanio_terreno > 0.
+*  Post-condiciones: Se agregan los fuegos a los obstaculos del nivel.
+*/
 void generar_fuegos(int cantidad_fuegos, nivel_t* nivel, int tamanio_terreno){
     coordenada_t coordenada;
 
@@ -113,6 +141,11 @@ void generar_fuegos(int cantidad_fuegos, nivel_t* nivel, int tamanio_terreno){
     
 }
 
+/* Pre-condiciones: nivel ya inicializado con sus paredes, posicion inicial del jugador y 
+*                   los obstaculos de tipo fuego.
+*      cantidad_medias >= 0 y tamaño_terreno > 0.
+*  Post-condiciones: Se agregan las medias a los obstaculos del nivel.
+*/
 void generar_medias(int cantidad_medias, nivel_t* nivel, int tamanio_terreno){
     coordenada_t coordenada;
     int indice_inicial = nivel->tope_obstaculos;
@@ -133,6 +166,11 @@ void generar_medias(int cantidad_medias, nivel_t* nivel, int tamanio_terreno){
     }
 }
 
+/* Pre-condiciones: nivel ya inicializado con sus paredes, posicion inicial del jugador y 
+*                  los obstaculos (fuegos y medias).
+*                  cantidad_botellas >= 0 y tamaño_terreno > 0.
+*  Post-condiciones: Se agregan las botellas a las herramientas del nivel.
+*/
 void generar_botellas_gritos(int cantidad_botellas, nivel_t* nivel, int tamanio_terreno){
     coordenada_t coordenada;
 
@@ -152,6 +190,11 @@ void generar_botellas_gritos(int cantidad_botellas, nivel_t* nivel, int tamanio_
     }
 }
 
+/* Pre-condiciones: nivel inicializado con sus paredes, posicion inicial del jugador,
+*                  obstaculos (fuegos y medias) y herramientas de tipo botella de grito.
+*      tamaño_terreno > 0.
+*  Post-condiciones: Se agregan los interruptores a las herramientas del nivel.
+*/
 void generar_interruptores(int cantidad_interruptores, nivel_t* nivel, int tamanio_terreno){
     coordenada_t coordenada;
     int indice_inicial = nivel->tope_herramientas;
@@ -173,6 +216,9 @@ void generar_interruptores(int cantidad_interruptores, nivel_t* nivel, int taman
     }
 }
 
+/* Pre-condiciones: nivel inicializado y número_nivel ∈ {1,2,3}.
+*  Post-condiciones: Se generan las herramientas correspondientes al número de nivel indicado.
+*/
 void generar_herramientas(nivel_t* nivel, int numero_nivel, int tamanio_terreno){
     if (numero_nivel == 1){
         nivel->tope_herramientas = 0;
@@ -189,6 +235,11 @@ void generar_herramientas(nivel_t* nivel, int numero_nivel, int tamanio_terreno)
     }
 }
 
+/* Pre-condiciones: nivel inicializado con sus paredes, posicion inicial del jugador,
+*                   obstaculos (fuegos y medias) y 
+*                   herramientas (botellas de gritos e interruptores).
+*  Post-condiciones: Se agregan los papeleos al nivel.
+*/
 void generar_papeleos(nivel_t* nivel, int cantidad_papeleos, int tamanio_terreno){
     nivel->tope_papeleos = 0;
     coordenada_t coordenada;
@@ -211,6 +262,9 @@ void generar_papeleos(nivel_t* nivel, int cantidad_papeleos, int tamanio_terreno
     }
 }
 
+/* Pre-condiciones: cantidad_movimientos >= 0, cantidad_martillos >= 0, cantidad_extintores >= 0
+*  Post-condiciones: Se inicializan los datos del jugador con los valores pasados por parámetro.
+*/
 void inicalizar_datos_jugador(jugador_t* jugador, int cantidad_movimientos, int cantidad_martillos, 
     int cantidad_extintores, bool interruptor){
         jugador->movimientos = cantidad_movimientos;
@@ -313,6 +367,9 @@ void inicializar_juego(juego_t* juego, char amigo_pide_favor){
     }
 }
 
+/* Pre-condiciones: terreno inicializado y nivel inicializados.
+*  Post-condiciones: Se cargan las paredes correspondientes al nivel pasado como parámetro.
+*/
 void cargar_paredes(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], nivel_t nivel){
     int fil = 0;
     int col = 0;
@@ -325,11 +382,17 @@ void cargar_paredes(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], nivel_t nive
     }
 }
 
+/* Pre-condiciones: terreno y posicion_jugador inicializados.
+*  Post-condiciones: asigna la posicion del jugador en el terreno.
+*/
 void cargar_posicion_jugador(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], 
     coordenada_t posicion_jugador){
         terreno[posicion_jugador.fil][posicion_jugador.col] = MIKE;
 }
 
+/* Pre-condiciones: terreno y nivel inicializados.
+*  Post-condiciones: Se cargan los obstáculos correspondientes al nivel pasado como parámetro.
+*/
 void cargar_obstaculos(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], nivel_t nivel){
     int fil = 0;
     int col = 0;
@@ -346,6 +409,9 @@ void cargar_obstaculos(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], nivel_t n
     }
 }
 
+/* Pre-condiciones: terreno y nivel inicializados.
+*  Post-condiciones: Se cargan todas las herramientas correspondientes al nivel pasado como parámetro.
+*/
 void cargar_herramientas(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], nivel_t nivel){
     int fil = 0;
     int col = 0;
@@ -362,6 +428,9 @@ void cargar_herramientas(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], nivel_t
     }
 }
 
+/* Pre-condiciones: terreno y nivel inicializados.
+*  Post-condiciones: Se cargan los papeleos correspondientes al nivel pasado como parámetro.
+*/
 void cargar_papeleos(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], nivel_t nivel){
     int fil = 0;
     int col = 0;
@@ -374,6 +443,9 @@ void cargar_papeleos(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], nivel_t niv
     }
 }
 
+/* Pre-condiciones: tamanio_terreno ∈ {12, 17, 22}
+*  Post-condiciones: Genera el terreno en funcion de tamanio_terreno.
+*/
 void generar_terreno(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], int tamanio_terreno){
     for (int i = 0; i < tamanio_terreno; i++){
         for (int j = 0; j < tamanio_terreno; j++){
@@ -382,6 +454,10 @@ void generar_terreno(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], int tamanio
     }
 }
 
+/* Pre-condiciones: terreno y juego inicializados.
+*  Post-condiciones: Copia toda la información del nivel a la matriz 'terreno', 
+*        dejándola consistente para su posterior impresión.
+*/
 void cargar_terreno(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], juego_t juego){
     cargar_paredes(terreno, juego.niveles[juego.nivel_actual]);
     cargar_posicion_jugador(terreno, juego.niveles[juego.nivel_actual].pos_inicial_jugador);
@@ -390,6 +466,9 @@ void cargar_terreno(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], juego_t jueg
     cargar_papeleos(terreno, juego.niveles[juego.nivel_actual]);
 }
 
+/* Pre-condiciones: 'terreno' previamente cargado y tamanio_terreno ∈ {12, 17, 22}.
+*  Post-condiciones: Imprime por pantalla el mapa completo con su contenido. 
+*/
 void imprimir_mapa(char terreno[TERRENO_NIVEL_1][TERRENO_NIVEL_1], int tamanio_terreno){
     for (int i = 0; i < tamanio_terreno; i++){
         for (int j = 0; j < tamanio_terreno; j++){
